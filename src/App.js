4c02c61cd7e655ee610210f780128f8cf94ed696
Chikezie-Joachim <Chukwudi2726.cj@gmail.com>
2020-12-26 13:55:30 -0800

    Added Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.scss";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Aside from "./components/Aside";
+import Home from "./pages/Home";
+import Fetch from "./pages/Fetch";
+import Axios from "./pages/Axios";
+import Apollo from "./pages/Apollo";
 
 function App() {
   return (
@@ -12,7 +16,12 @@ function App() {
         <aside className="aside_">
           <Aside />
         </aside>
-        <main className="main_"></main>
+        <main className="main_">
+          <Route path="/" exact={true} component={Home}></Route>
+          <Route path="/fetch" component={Fetch}></Route>
+          <Route path="/axios" component={Axios}></Route>
+          <Route path="/apollo" component={Apollo}></Route>
+        </main>
       </section>
     </Router>
   );
